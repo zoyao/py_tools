8312a18d1f787d569859129cf9320f2c3c7c85aa
zoyao <zhongyao93@gmail.com>
2023-07-14 11:13:46 +0800

    代码注释

--- check_tool/lukou3.py
+++ check_tool/lukou3.py
@@ -43,6 +43,7 @@ class lukou(object):
             base_url = 'https://www.lukou.com/service/feeds'
             start += 10
             if type(result) == str:
+                # 兼容下拉加载数据，下拉数据以json方式返回
                 soup = BeautifulSoup(result, 'html.parser')
                 soup_list = soup.findAll('div', class_='feed-wrap')
                 list = []
@@ -57,11 +58,13 @@ class lukou(object):
                     x = ElementY(absolute_links, text)
                     list.append(x)
             else:
+                # 获取页面数据
                 list = result.html.find('.feed-wrap')
             for x in list:
                 for url in x.absolute_links:
                     if 'lukou' in url and 'userfeed' in url and '#' not in url:
                         break
+                # 根据url读取商品id
                 if '?' in url:
                     id_str = url[url.index('userfeed/') + 9:url.index('?')]
                 else:
@@ -73,11 +76,12 @@ class lukou(object):
                     id = 0
                     logging.error(url)
                     logging.error(x.absolute_links)
-
                 if id <= begin_end_id:
+                    # 已读取过商品，直接退出
                     logging.info('end')
                     return
                 if first_flag:
+                    # 记录第一个商品的id，后续下拉获取及翻页接口需要
                     first_flag = False
                     end_id = id
                     if end_id > self.end_id:
@@ -85,27 +89,35 @@ class lukou(object):
                 detail = x.text
                 # auth = x.find('.author')
                 words = '【无】'
+                # 检查是否有匹配内容
                 for c in self.check_list:
                     user = c['user']
                     flag = False
                     if 'keywords' in c.keys():
                         for keys in c['keywords']:
+                            # keywords 内任意满足即可
                             words = ''
                             key_flag = True
                             for key in keys.split(','):
+                                # 以','分割内容需全部满足
                                 words += '【' + key + '】'
                                 if key not in detail:
+                                    # 匹配失败
                                     key_flag = False
                                     break
                             if key_flag:
+                                # 匹配成功
                                 flag = True
                                 break
                     else:
+                        # keywords 无数据，全部推送
                         flag = True
                     if flag:
+                        # 调用通知接口
                         self.notice.send_notice(user, '路口关键词匹配成功' + words, detail + '<' + url + '>')
             format_flag = True
             if start >= 50:
+                # 翻页
                 start = 10
                 page += 1
                 base_url = 'https://www.lukou.com/circle'
@@ -115,4 +127,5 @@ class lukou(object):
                 return
             result = self.session.get(base_url + '?end_id=' + str(end_id) + '&page=' + str(page) + '&start=' + str(start))
             if format_flag:
+                # 兼容下拉加载数据，下拉数据以json方式返回
                 result = json.loads(result.html.html).get('html')
