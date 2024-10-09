43aa64048212d91889c1af4c530429a56fe749ee
zoyao <zhongyao93@gmail.com>
2024-10-09 09:23:20 +0800

    Update user_detail.py
    异常捕获

--- xhs/user_detail.py
+++ xhs/user_detail.py
@@ -55,67 +55,69 @@ def run(pw: Playwright) -> None:
         for item in results:
             user_id = item['id']
             print(url + user_id)
-            page.goto(url + user_id)
+            try:
+                page.goto(url + user_id)
 
-            user_name = None
-            user_code = None
-            user_ip = None
-            user_tag = None
-            follow_count = 0
-            fans_count = 0
-            like_count = 0
+                user_name = None
+                user_code = None
+                user_ip = None
+                user_tag = None
+                follow_count = 0
+                fans_count = 0
+                like_count = 0
 
-            user_name_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > '
-                                         'div.basic-info > div.user-basic > div.user-nickname > div').all()
-            if len(user_name_all) > 0:
-                user_name = user_name_all[0].text_content()
-            user_code_all = page.locator('#userPageContainer > div.user > div > div.info-part '
-                                         '> div.info > div.basic-info > div.user-basic > div.user-content > '
-                                         'span.user-redId').all()
-            if len(user_code_all) > 0:
-                user_code = user_code_all[0].text_content().replace('小红书号：', '')
-            user_ip_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > '
-                                       'div.basic-info > div.user-basic > div.user-content > span.user-IP').all()
-            if len(user_ip_all) > 0:
-                user_ip = user_ip_all[0].text_content().replace(' IP属地：', '')
-            user_tag_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > '
-                                        'div.user-tags > div').all()
-            if len(user_tag_all) > 0:
-                user_tag = ''
-                for tag in user_tag_all:
-                    user_tag += tag.text_content()
-            count_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > div.data-info > '
-                                     'div > div').all()
-            if len(count_all) > 0:
-                for count_item in count_all:
-                    span_count_all = count_item.locator('span.count').all()
-                    span_show_all = count_item.locator('span.shows').all()
-                    if len(span_show_all) > 0 and len(span_count_all) > 0:
-                        span_show = span_show_all[0].text_content()
-                        span_count = span_count_all[0].text_content()
-                        if '关注' == span_show:
-                            follow_count = get_span_count(span_count)
-                        elif '粉丝' == span_show:
-                            fans_count = get_span_count(span_count)
-                        elif '获赞与收藏' == span_show:
-                            like_count = get_span_count(span_count)
-            pass
-            insert_query = """
-                                update bs_user_xhs
-                                set code = %s,
-                                name = %s,
-                                ip_address = %s,
-                                tags = %s,
-                                follow_num = %s,
-                                fans_num = %s,
-                                like_num = %s,
-                                update_time = CURRENT_TIMESTAMP
-                                where id = %s
-                            """
-            values = (user_code, user_name, user_ip, user_tag, follow_count, fans_count, like_count, user_id)
-            cursor.execute(insert_query, values)
-            conn.commit()
-
+                user_name_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > '
+                                             'div.basic-info > div.user-basic > div.user-nickname > div').all()
+                if len(user_name_all) > 0:
+                    user_name = user_name_all[0].text_content()
+                user_code_all = page.locator('#userPageContainer > div.user > div > div.info-part '
+                                             '> div.info > div.basic-info > div.user-basic > div.user-content > '
+                                             'span.user-redId').all()
+                if len(user_code_all) > 0:
+                    user_code = user_code_all[0].text_content().replace('小红书号：', '')
+                user_ip_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > '
+                                           'div.basic-info > div.user-basic > div.user-content > span.user-IP').all()
+                if len(user_ip_all) > 0:
+                    user_ip = user_ip_all[0].text_content().replace(' IP属地：', '')
+                user_tag_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > '
+                                            'div.user-tags > div').all()
+                if len(user_tag_all) > 0:
+                    user_tag = ''
+                    for tag in user_tag_all:
+                        user_tag += tag.text_content()
+                count_all = page.locator('#userPageContainer > div.user > div > div.info-part > div.info > div.data-info > '
+                                         'div > div').all()
+                if len(count_all) > 0:
+                    for count_item in count_all:
+                        span_count_all = count_item.locator('span.count').all()
+                        span_show_all = count_item.locator('span.shows').all()
+                        if len(span_show_all) > 0 and len(span_count_all) > 0:
+                            span_show = span_show_all[0].text_content()
+                            span_count = span_count_all[0].text_content()
+                            if '关注' == span_show:
+                                follow_count = get_span_count(span_count)
+                            elif '粉丝' == span_show:
+                                fans_count = get_span_count(span_count)
+                            elif '获赞与收藏' == span_show:
+                                like_count = get_span_count(span_count)
+                pass
+                insert_query = """
+                                    update bs_user_xhs
+                                    set code = %s,
+                                    name = %s,
+                                    ip_address = %s,
+                                    tags = %s,
+                                    follow_num = %s,
+                                    fans_num = %s,
+                                    like_num = %s,
+                                    update_time = CURRENT_TIMESTAMP
+                                    where id = %s
+                                """
+                values = (user_code, user_name, user_ip, user_tag, follow_count, fans_count, like_count, user_id)
+                cursor.execute(insert_query, values)
+                conn.commit()
+            except Exception as e:
+                print(e)
     except Exception as e:
         print(e)
     finally:
